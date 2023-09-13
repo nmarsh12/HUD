5d873187c9eb1531a24f765c0f58afb77017e9c8
nmarsh12 <nathanmarsh6283@gmail.com>
2023-09-13 14:36:55 -0300

    Added Drawing and Input Detection

--- HUD/Program.cs
+++ HUD/Program.cs
@@ -8,10 +8,32 @@ namespace HUD
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            List<String> screen;
+            screen = new List<String> { "a", "b" };
+
+            ConsoleKey ReadInput()
+            {
+                return Console.ReadKey(true).Key;
+            }
+            
+            void DrawScreen()
+            {
+                Console.Clear();
+                foreach (string line in screen)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                DrawScreen();
+                ReadInput();
+            }
         }
     }
 }
