db22b3933516fa1b8245d925dd4445321e659633
nmarsh12 <nathanmarsh6283@gmail.com>
2023-09-18 09:43:47 -0300

    More Functions
    Everything up to starting the options menu

--- HUD/Program.cs
+++ HUD/Program.cs
@@ -12,46 +12,96 @@ namespace HUD
         {
             int playerMaxHealth = 100;
             int playerCurrentHealth;
-            string healthStatus;
+            string healthStatus = "Alive";
             int playerMaxShield = 100;
             int playerCurrentShield;
 
+            // This is the position of the pointer for the menu arrow thing to move it around
+            int optionSelectIndex = 0;
+
+            string playerHealthDisplay;
+            string playerShieldDisplay;
+            string playerStatusDisplay;
+            string quitInfo;
+
             List<String> screen;
-            screen = new List<String> { "a", "b" };
             bool running = true;
             ConsoleKey currentInput;
 
+            // Functions
             ConsoleKey ReadInput()
             {
                 return Console.ReadKey(true).Key;
             }
+
+            void OnStart()
+            {
+                playerCurrentHealth = playerMaxHealth;
+                playerCurrentShield = playerMaxShield;
+            }
+
+            List<String> OptionsMenu()
+            {
+
+            }
+
+            void OptionsController(string[] options)
+            {
+
+            }
            
             void FillScreen(){
-                 screen = new List<String>{
-                    
-               }; 
+                playerHealthDisplay = "Health: " + playerCurrentHealth + "/" + playerMaxHealth;
+                playerShieldDisplay = "Shield: " + playerCurrentShield + "/" + playerMaxShield;
+                playerStatusDisplay = "Player is " + healthStatus;
+                quitInfo = "Press Escape Key to Quit";
+
+                screen = new List<String>{
+                    playerHealthDisplay,
+                    playerShieldDisplay,
+                    playerStatusDisplay,
+                    "",
+                    quitInfo
+                }; 
             }
 
             void DrawScreen()
             {
                 Console.Clear();
-                foreach (string line in screen)
+                foreach ( string line in screen )
                 {
-                    Console.WriteLine(line);
+                    Console.WriteLine( line );
                 }
             }
 
-            for (int i = 0; i < 5; i++)
+            void ManageInput()
             {
-                DrawScreen();
-                ReadInput();
+                currentInput = ReadInput();
+
+                switch (currentInput) {
+                    case ConsoleKey.Escape:
+                        running = false;
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.A:
+                        optionSelectIndex -= 1; break;
+
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.D:
+                        optionSelectIndex += 1; break;
+                }
             }
+
+            // Game Loop
+
+            OnStart();
 
             while (running) {
-                currentInput = ReadInput();
-                if ( currentInput == ConsoleKey.Q ) {
-                   running = false;
-                }
+                FillScreen();
+                DrawScreen();
+                Console.WriteLine(optionSelectIndex);
+                ManageInput();
             }
         }
     }
